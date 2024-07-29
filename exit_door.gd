9faf9346a0754e9c309d6ba1ba48ce98c5469583
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-07-29 14:52:30 +0200

    Create exit_door.gd

---
+++ exit_door.gd
@@ -0,0 +1,48 @@
+extends Interactable
+
+var playback : AnimationNodeStateMachinePlayback
+var open = false
+var closed = true
+var locked = false
+var can_interact = true
+
+@onready var timer = $"../../Timer"
+@onready var animationplayer = $"../../AnimationPlayer"
+
+
+func _ready():
+	pass
+	#playback = $"../../AnimationTree".get("parameters/playback")
+
+
+func _on_interacted(body):
+	if can_interact == true:
+		if locked == false:
+			if open == false:
+				can_interact = false
+				open = true
+				closed = false
+				animationplayer.play("Open")
+				await get_tree().create_timer(2.5).timeout
+				can_interact = true
+			if closed == false:
+				can_interact = false
+				animationplayer.play("Close")
+				closed = true
+				open = false
+				await get_tree().create_timer(1.5).timeout
+				can_interact = true
+		else:
+			can_interact = false
+			animationplayer.play("Locked_open_attempt")
+			await get_tree().create_timer(0.5).timeout
+			can_interact = true
+
+
+func _on_area_3d_player_has_exited():
+	can_interact = false
+	locked = true
+	if closed == false:
+		animationplayer.play("Close")
+	await get_tree().create_timer(1.0).timeout
+	can_interact = true
