5a8519837890b4fabeb7230e41a956e054292e25
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-07-30 01:54:47 +0200

    Create main_map.gd

---
+++ main_map.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@onready var Interior01 = $Interior_01
+var hidden = false
+var showing = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#func _input(event):
+	#if showing == true:
+		#if Input.is_action_just_pressed("interact"):
+			#Interior01.hide()
+			#hidden = true
+			#showing = false
+	#else:
+		#if Input.is_action_just_pressed("interact"):
+			#Interior01.show()
+			#hidden = false
+			#showing = true
