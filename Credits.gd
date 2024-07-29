bceddaff7bba9f9dfe3511a8edc443375b7f301b
Sander C. Braathen <sanderbraathen67@gmail.com>
2024-07-30 01:54:06 +0200

    Update Credits.gd

--- Credits.gd
+++ Credits.gd
@@ -6,11 +6,11 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	fade.play("Fade_Back")
-	slide_01.show()
-	await get_tree().create_timer(3.0).timeout
-	fade.play("Fade_Black")
-	await get_tree().create_timer(1.5).timeout
-	slide_01.hide()
-	fade.play("Fade_Back")
+#func _ready():
+	#fade.play("Fade_Back")
+	#slide_01.show()
+	#await get_tree().create_timer(3.0).timeout
+	#fade.play("Fade_Black")
+	#await get_tree().create_timer(1.5).timeout
+	#slide_01.hide()
+	#fade.play("Fade_Back")
